c5b4fd7778ecbb551ffaa18a2372f8f6db3f80a3
Duc Dinh <holahu9@gmail.com>
2021-03-31 17:30:58 -0400

    add app.js,index.js

---
+++ src/components/Header/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+
+function Header() {
+    return (
+        <div className="header text-center">
+                <h1>Employee Directory</h1> <br></br>
+                <p className = "pstyle">Click on each heading to filter or use the search box to narrow your results</p>
+                
+
+
+        </div>
+    );
+}
+export default Header
